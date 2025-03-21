de8c8808ea2af98f6ba56a9f86c3825537f77b69
vdsrodrigo <vds.rodrigo@gmail.com>
2025-03-21 15:45:00 -0300

    Add BSON mapping annotations to Ledger entity
    Introduced BSON attributes to map Ledger properties for MongoDB serialization. This ensures proper field naming and compatibility when persisting or retrieving data from the database.

--- src/ToolBox/Domain/Entities/Ledger.cs
+++ src/ToolBox/Domain/Entities/Ledger.cs
@@ -1,14 +1,21 @@
+using MongoDB.Bson.Serialization.Attributes;
 using ToolBox.Domain.Exceptions;
 
 namespace ToolBox.Domain.Entities;
 
 public class Ledger
 {
+    [BsonElement("cpf")]
     public string Cpf { get; private set; }
+    [BsonElement("createdAt")]
     public DateTime CreatedAt { get; private set; }
+    [BsonElement("ledgerTypeId")]
     public int LedgerTypeId { get; private set; }
+    [BsonElement("points")]
     public int? Points { get; private set; }
+    [BsonElement("pointsBlocked")]
     public int PointsBlocked { get; private set; }
+    [BsonElement("status")]
     public string Status { get; private set; }
 
     private Ledger() { } // Para o MongoDB
